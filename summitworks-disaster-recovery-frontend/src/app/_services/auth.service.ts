c14d4b99271f3440a3db925a5ab44f0957d9b28b
Shawn11564 <shawnmicheldev@gmail.com>
2022-05-31 13:22:22 -0400

    Initial file structure, added post methods in AuthService for login/register

---
+++ summitworks-disaster-recovery-frontend/src/app/_services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'http://localhost:8080/api/auth/';
+const HTTP_OPTIONS = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(AUTH_API + 'login', {
+      username: credentials.username,
+      password: credentials.password
+    }, HTTP_OPTIONS);
+  }
+
+  register(user: any): Observable<any> {
+    return this.http.post(AUTH_API + 'register', {
+      username: user.username,
+      email: user.email,
+      password: user.password
+    }, HTTP_OPTIONS);
+  }
+
+}
