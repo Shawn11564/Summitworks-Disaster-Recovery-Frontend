762f89e4cb5a074d69d24b2334f51ae2816e1bdc
Shawn11564 <shawnmicheldev@gmail.com>
2022-05-31 20:51:22 -0400

    Implemented registering from webpage

--- summitworks-disaster-recovery-frontend/src/app/login/login.component.ts
+++ summitworks-disaster-recovery-frontend/src/app/login/login.component.ts
@@ -34,8 +34,7 @@ export class LoginComponent implements OnInit {
         this.isLoggedIn = true;
         this.roles = this.tokenStorage.getUser().roles;
         this.reloadPage();
-      },
-      err => {
+      }, err => {
         this.errorMessage = err.error.message;
         this.isLoginFailed = true;
       }
--- summitworks-disaster-recovery-frontend/src/app/register/register.component.ts
+++ summitworks-disaster-recovery-frontend/src/app/register/register.component.ts
@@ -9,10 +9,26 @@ import { TokenStorageService } from '../_services/token-storage.service';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+  form: any = {};
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
-  ngOnInit(): void {
-    
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void { }
+
+  onSubmit(): void {
+    this.authService.register(this.form).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      }, err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    )
   }
 
 }
